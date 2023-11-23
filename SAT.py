9af920ed730878ccadc704bc9ece3bc20f3f8eb1
Olaniyi Charles Ogungbite <olaniyi2oguns@gmail.com>
2023-11-23 21:07:28 +0000

    Create SAT.py
    simple regression model

---
+++ SAT.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
